81bfd7c0264b2488e5f2c5cafa37712db25c966e
Shahid <82824696+shahid-orion@users.noreply.github.com>
2024-07-14 10:12:03 +1000

    add RouterLink and useRouter for navigation between pages

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,8 +1,22 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, ref, computed } from 'vue'
+import { RouterLink } from 'vue-router'
 
-defineProps({
+const props = defineProps({
   job: Object,
+})
+
+const showFullDescription = ref(false)
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description
+  if (!showFullDescription.value) {
+    description = description.substring(0, 100) + '...'
+  }
+  return description
 })
 </script>
 
@@ -15,9 +29,16 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        We are seeking a talented Front-End Developer to join our team in
-        Boston, MA. The ideal candidate will have strong skills in HTML, CSS,
-        and JavaScript...
+        <!-- {{ job.description }} -->
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button
+          @click="toggleFullDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? 'Show Less' : 'Show More' }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -26,15 +47,15 @@ defineProps({
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
-        <a
-          :href="`/job/${job.id}`"
+        <RouterLink
+          :to="`/job/${job.id}`"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
